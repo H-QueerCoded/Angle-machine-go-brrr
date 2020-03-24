933092b152ced7ff04352640b2aa0120fb541c16
Hogan S <37481947+Hogan-Dazs@users.noreply.github.com>
2020-03-24 12:08:21 -0500

    feat: input loop

--- src/angleMachine.java
+++ src/angleMachine.java
@@ -1,4 +1,25 @@
+import javax.swing.JOptionPane;
 
 public class angleMachine {
-
+	private static Object[] possibleValues = { "Rectangular", "Polar"};
+	
+	public static void main(String[] args) {
+		Object selectedValue = message();
+		while(selectedValue!=null) {
+			if(selectedValue == "Rectangular") {
+				
+			}
+			else {
+				
+			}
+			selectedValue = message();
+		}
+	}
+	
+	private static Object message() {
+		return JOptionPane.showInputDialog(null,
+		        "Select mode", "Input",
+		        JOptionPane.INFORMATION_MESSAGE, null,
+		        possibleValues, possibleValues[0]);
+	}
 }
