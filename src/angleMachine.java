04364871686acfac1659025fbefb217f683621d2
Hogan S <37481947+Hogan-Dazs@users.noreply.github.com>
2020-03-25 22:23:15 -0500

    feat: polar division & multiplication

--- src/angleMachine.java
+++ src/angleMachine.java
@@ -5,7 +5,7 @@ public class angleMachine {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int selectedValue = consIn(sc);
-		while(selectedValue == 1 || selectedValue == 2){
+		while(selectedValue == 1 || selectedValue == 2 || selectedValue == 3 || selectedValue == 4){
 			if(selectedValue == 1) {
 				double[] input = input1(sc);
 				if(input!=null) {
@@ -28,13 +28,42 @@ public class angleMachine {
 					System.out.println(out);
 				}
 			}
+			else if(selectedValue == 3) {
+				double[] input = input3(sc);
+				if(input!=null) {
+					double v1 = input[0];
+					double a1 = input[1];
+					double v2 = input[2];
+					double a2 = input[3];
+					double vector = v1*v2;
+					double angle = a1+a2;
+					String out = "Vector: "+vector+"\nAngle: "+angle+"\n";
+					System.out.println(out);
+				}
+			}
+			else if(selectedValue == 4) {
+				double[] input = input3(sc);
+				if(input!=null) {
+					double v1 = input[0];
+					double a1 = input[1];
+					double v2 = input[2];
+					double a2 = input[3];
+					double vector = v1/v2;
+					double angle = a1-a2;
+					String out = "Vector: "+vector+"\nAngle: "+angle+"\n";
+					System.out.println(out);
+				}
+			}
 			selectedValue = consIn(sc);
 		}
 		sc.close();
 	}
 	
 	private static int consIn(Scanner scan) {
-		System.out.print("Polar to Rectangular: 1 \nRectangular to Polar: 2\nOther to exit\nMake a selection: ");
+		System.out.print("Polar to Rectangular: 1 "
+				+ "\nRectangular to Polar: 2"
+				+ "\nPolar Multiplication: 3\nPolar Division:	      4"
+				+ "\nOther to exit\nMake a selection: ");
 	    int i = scan.nextInt();
 		return i;
 	}
@@ -52,6 +81,18 @@ public class angleMachine {
 		input[0] = sc.nextDouble();
 		System.out.print("Enter Non-Real Value: ");
 		input[1] = sc.nextDouble();
+		return input;
+	}
+	private static double[] input3(Scanner sc) {
+		double[] input = new double[4];
+		System.out.print("Enter Value 1: ");
+		input[0] = sc.nextDouble();
+		System.out.print("Enter Angle 1: ");
+		input[1] = sc.nextDouble();
+		System.out.print("Enter Value 2: ");
+		input[2] = sc.nextDouble();
+		System.out.print("Enter Angle 2: ");
+		input[3] = sc.nextDouble();
 		return input;
 	}
 }
