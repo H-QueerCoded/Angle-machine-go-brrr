a99b7133eaa9f1c79ede79dab80b7c26ca87677d
Hogan S <37481947+Hogan-Dazs@users.noreply.github.com>
2020-03-25 21:19:32 -0500

    feat: Rectangular to Polar

--- src/angleMachine.java
+++ src/angleMachine.java
@@ -5,40 +5,42 @@ import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 public class angleMachine {
-	private static Object[] possibleValues = { "Rectangular", "Polar"};
 	
 	public static void main(String[] args) {
-		Object selectedValue = message();
-		while(selectedValue!=null) {
-			if(selectedValue == "Rectangular") {
-				double[] input = input();
+		int selectedValue = Integer.parseInt(message().toString());
+		while(selectedValue == 1 || selectedValue == 2) {
+			if(selectedValue == 1) {
+				double[] input = input1();
 				if(input!=null) {
 					double value = input[0];
 					double angle = input[1];
-					double cos = value*Math.cos(angle);
-					double sin = value*Math.sin(angle);
+					double cos = value*Math.cos(Math.toRadians(angle));
+					double sin = value*Math.sin(Math.toRadians(angle));
 					String out = "Cosine: "+cos+"\nSine: "+sin;
 					JOptionPane.showMessageDialog(null, out);
 				}
 			}
-			else {
-				double[] input = input();
+			else if(selectedValue == 2) {
+				double[] input = input2();
 				if(input!=null) {
-					double value = input[0];
-					double angle = input[1];
+					double one = input[0];
+					double two = input[1];
+					double vector = Math.sqrt((one*one)+(two*two));
+					double angle = Math.toDegrees(Math.atan(two/one));
+					String out = "Vector: "+vector+"\nAngle: "+angle;
+					JOptionPane.showMessageDialog(null, out);
 				}
 			}
-			selectedValue = message();
+			selectedValue = Integer.parseInt(message().toString());
 		}
 	}
 	
 	private static Object message() {
 		return JOptionPane.showInputDialog(null,
-		        "Select mode", "Input",
-		        JOptionPane.INFORMATION_MESSAGE, null,
-		        possibleValues, possibleValues[0]);
+		        "Polar to Rectangular: 1 \nRectangular to Polar: 2", "Input",
+		        JOptionPane.INFORMATION_MESSAGE, null,null,null);
 	}
-	private static double[] input() {
+	private static double[] input1() {
 		JTextField value = new JTextField(10);
 	    JTextField angle = new JTextField(10);
 		JPanel myPanel = new JPanel();
@@ -48,7 +50,27 @@ public class angleMachine {
 	      myPanel.add(new JLabel("Angle:"));
 	      myPanel.add(angle);
 	      int result = JOptionPane.showConfirmDialog(null, myPanel, 
-	               "Please Enter Input", JOptionPane.OK_CANCEL_OPTION);
+	               "Polar to Rectangular", JOptionPane.OK_CANCEL_OPTION);
+	      if(result == JOptionPane.OK_OPTION) {
+		      double[] arr = new double[2];
+		      arr[0] = Double.parseDouble(value.getText());
+		      arr[1] = Double.parseDouble(angle.getText());
+		      return arr;
+	      }
+	      else
+	    	  return null;
+	}
+	private static double[] input2() {
+		JTextField value = new JTextField(10);
+	    JTextField angle = new JTextField(10);
+		JPanel myPanel = new JPanel();
+	      myPanel.add(new JLabel("Real:"));
+	      myPanel.add(value);
+	      myPanel.add(Box.createHorizontalStrut(15)); // a spacer
+	      myPanel.add(new JLabel("Non-real:"));
+	      myPanel.add(angle);
+	      int result = JOptionPane.showConfirmDialog(null, myPanel, 
+	               "Rectangular to Polar", JOptionPane.OK_CANCEL_OPTION);
 	      if(result == JOptionPane.OK_OPTION) {
 		      double[] arr = new double[2];
 		      arr[0] = Double.parseDouble(value.getText());
